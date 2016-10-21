10f43b0cf66f4420623ae32f563154abaecfd7b2
Rory Ashford <rory@roikles.com>
2016-10-21 16:57:57 +0100

    Init commit

---
+++ app/scripts.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var ReactAnimations = require('react-addons-css-transition-group');
+
+import { hashHistory } from 'react-router';
+//import Home from './components/home';
+
+var wrapper = document.querySelector("#app");
+
+var { 
+    Router,
+    Route,
+    IndexRoute,
+    IndexLink,
+    Link 
+} = ReactRouter;
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Hello world
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="anotherpage" component={AnotherPage} />
+        </Route>
+    </Router>,
+    wrapper
+);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# React Starter
+
+I made this to better understand the React stack myself rather than using another persons react setup and then spending an eternity debugging because I dont understand what is going on.
+
+I emplore you to do the same =)
